9224a92e130a7e0e3bea18ff93e637d23f2d6bf3
etdickey <etdickey@gmail.com>
2017-09-21 00:04:02 -0500

    Initial commit for project.

---
+++ Math Problem/src/Math Problem.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Math.cpp
+// Author      : Ethan Dickey
+// Version     :
+// Copyright   : Your copyright notice
+// Description : This program attempts to find ALL (so no random guessing)
+//solutions to this problem in a more efficient manner than simply going
+//through all possible numbers between 0 and 1 billion:::
+//Rule: the 9 numbers (n1-n9) cannot repeat each other.
+//Find the solutions to this equation: (n1/n2*n3+n4*n5*n6/n7+n8*n9)=100
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
