b7c25ba8484075034b85e3f26725df9f3c202e02
Josh <Josh@129.62.126.167>
2017-09-21 16:34:41 -0500

    Added code from other project

--- MathProblem/main.cpp
+++ MathProblem/main.cpp
@@ -13,13 +13,73 @@
 
 #include <cstdlib>
 
-using namespace std;
+//============================================================================
+// Name        : NineDifferentNumbers.cpp
+// Author      : Josh Holland
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
 
-/*
- * 
+/**
+ *  ** This program goes through the 1,2,3,4,5,6,7,8,9 number
+ *  		sequence and makes all the possible combinations
+ *  		of these numbers without using one number twice
+ *
+ *
+ * 1. There will be 9! (362880) different numbers tested
+ * 2.
+ *
+ *
  */
-int main(int argc, char** argv) {
 
-    return 0;
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+const int NUMBER_OF_NUMBERS=9;
+const int ORIGIN[] = {1,2,3,4,5,6,7,8,9};
+
+void swap(int*first, int*second);
+
+void rotate(int* r[]);
+
+int main() {
+
+	int currentRotation[NUMBER_OF_NUMBERS];
+
+	for(int i=0;i<NUMBER_OF_NUMBERS;i++){
+		currentRotation[i]=ORIGIN[i];
+	}
+
+	int currentCombination[NUMBER_OF_NUMBERS];
+
+	enum positions{a,b,c,d,e,f,g,h,i};
+
+	for(int a=1;a<=9;a++){//position a will stop
+
+	}
+
+
+
+
+
+
+
+
+
+
+	return 0;
 }
+
+void swap(int*first, int*second){
+	int *temp=first;
+	second=first;
+	first=temp;
+}
+
+void rotate(int* r[]){
+
+}
+
+
 
